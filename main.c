38264f174f185071fd3e6952ac2f9b4602c9d228
Kauan <labsfiap@fiap.com.br>
2026-05-11 11:02:16 -0300

    add função exibir menu

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 # include <stdio.h>
+
+void exibir_menu(){
+    printf("\n=========================\n");
+    printf("SISTEMA CAIXA ELETRONICO");
+    printf("\n=========================\n");
+    printf("1 - Consultar Saldo\n");
+    printf("2 - Realizar Saque\n");
+    printf("3 - Realizar Deposito\n");
+    printf("0 - Sair\n");
+    printf("=========================\n");
+}
+
 int main (){
-    prinf('OI');
+    printf("OI");
 
 
 
